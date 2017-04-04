7a0fe78026814a98252c5d5aa56b261152f679c6
PaulBender <paulbender@live.com>
2017-04-04 11:42:59 -0400

    Removed PHP closing tags

--- inc/languages/english/limitregistration.lang.php
+++ inc/languages/english/limitregistration.lang.php
@@ -8,5 +8,3 @@ $l['limitregistration_info_name'] = "Limit Daily Registration";
 $l['limitregistration_info_desc'] = "Allows you to limit the number of registrations that can be made in a day.";
 
 $l['error_max_registration_day'] = "You cannot register as the Administrator has limited the number of registrations that can be made per day to {1}. Please try again later.";
-
-?>
--- inc/plugins/limitregistration.php
+++ inc/plugins/limitregistration.php
@@ -83,5 +83,3 @@ function limitregistration_run()
 		}
 	}
 }
-
-?>
